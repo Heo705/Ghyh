984cfcc1afb247725745b6067a0ab1df5fd7f84c
Heo705 <dongphamphuong1979@gmail.com>
2024-12-21 20:36:14 +0700

    Add files via upload

---
+++ test.lua
@@ -0,0 +1,6 @@
+Config = {
+    namescript = "Test",--Cá Heo
+    logoscript = "117390141608083",--ID Logo Script
+    tacgia = "sadas"-- Cá Heo8🐬 
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tngaming123/vesion/refs/heads/main/scriptbffree.lua"))()
